af7d92d4d3edde43b63006a1d0307ae040685cfa
vitalykamynin <vitaly.kamynin@gmail.com>
2024-04-16 22:47:09 -0400

    8

--- tests/health_check/test_DB_connection.py
+++ tests/health_check/test_DB_connection.py
@@ -2,7 +2,7 @@
 from tests.base_test import BaseTest
 
 import pytest
-import logging
+import logging as log
 
 
 @pytest.mark.db1
@@ -11,7 +11,8 @@ class TestConnectionDB(BaseTest):
     @pytest.mark.db2
     def test_checkDB_connection_1(self):
         result = self.get_db_user.random_email_from_DB(1)
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
+        log.debug(result[0])
         assert result[0]['group'] == 'APG777', f"Error"
 
     @pytest.mark.db3
