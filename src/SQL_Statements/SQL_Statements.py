1b2a1109f470ee5cd57bcf4e43fd79421013cb4d
Vitosgmailcom <126843827+Vitosgmailcom@users.noreply.github.com>
2024-04-18 03:10:32 +0000

    Deploying to gh-pages from @ Vitosgmailcom/askgit@9794533f538db897db30faddd789fd8e8745148d 🚀

--- src/SQL_Statements/SQL_Statements.py
+++
@@ -1,8 +0,0 @@
-
-class SqlStatements:
-    def random_user_sql(self):
-        return f"SELECT * FROM users WHERE `group`='APG777' order by id ASC LIMIT 10;"
-
-    def specific_user_sql(self, email):
-        return f"SELECT * FROM users WHERE `email`='{email}';"
-
--- tests/health_check/test_healthcheck.py
+++
@@ -1,7 +0,0 @@
-import logging as log
-import pytest
-
-@pytest.mark.health
-def test_healthcheck():
-    log.info("Hello world")
-
