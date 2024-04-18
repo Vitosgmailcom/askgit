3f4122a307acabc9b14ab1653010b1879e0e34fe
vitalykamynin <vitaly.kamynin@gmail.com>
2024-04-17 23:37:46 -0400

    3rd  push with Allure

--- tests/health_check/test_connectionDB.py
+++ tests/health_check/test_connectionDB.py
@@ -12,7 +12,7 @@ class TestConnectionDB(BaseTest):
     def test_checkDB_connection_1(self):
         result = self.db_helper.random_email_from_DB(1, self.sql.random_user_sql())
         log.info(result[0])
-        assert result[0]['group'] == 'APG77', f"Error"
+        assert result[0]['group'] == 'APG777', f"Error"
 
     @pytest.mark.db3
     def test_checkDB_connection(self):
